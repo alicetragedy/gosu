574c9eed2e6aaf9232a47b5dce215e60b8934107
alicetragedy <girl@alicetragedy.org>
2015-03-21 20:48:45 +0100

    add background

--- hello-world.rb
+++ hello-world.rb
@@ -3,6 +3,11 @@ require 'gosu'
     def initialize
       super(640, 480, false)
       self.caption = 'Hello World'
+      @background_image = Gosu::Image.new(self, "media/background.png", false)
+    end
+
+    def draw
+      @background_image.draw(0, 0, 0)
     end
   end
 
