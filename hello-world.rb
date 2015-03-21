65ea172bdbb414f51225b335ca35cd731211c833
alicetragedy <girl@alicetragedy.org>
2015-03-21 21:44:35 +0100

    add basic player

--- hello-world.rb
+++ hello-world.rb
@@ -1,12 +1,27 @@
 require 'gosu'
+
+  
+class Player
+  def initialize(window)
+    @image = Gosu::Image.new(window, "media/player.png", false)
+  end
+
+  def draw
+    @image.draw(0, 0, 1)
+  end
+end
+
   class GameWindow < Gosu::Window
     def initialize
-      super(640, 480, false)
+      super(800, 600, false)
       self.caption = 'Hello World'
       @background_image = Gosu::Image.new(self, "media/background.png", false)
+      @player = Player.new(self)
+      @player2 = Player2.new(self)
     end
 
     def draw
+      @player.draw
       @background_image.draw(0, 0, 0)
     end
   end
