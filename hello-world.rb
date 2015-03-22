53f2cb4583b53519732f9bbd5446d1f6887b3a30
alicetragedy <girl@alicetragedy.org>
2015-03-22 11:12:14 +0100

    fix: remove second object/player

--- hello-world.rb
+++ hello-world.rb
@@ -11,19 +11,18 @@ class Player
   end
 end
 
-  class GameWindow < Gosu::Window
-    def initialize
-      super(800, 600, false)
-      self.caption = 'Hello World'
-      @background_image = Gosu::Image.new(self, "media/background.png", false)
-      @player = Player.new(self)
-      @player2 = Player2.new(self)
-    end
-
-    def draw
-      @player.draw
-      @background_image.draw(0, 0, 0)
-    end
+class GameWindow < Gosu::Window
+  def initialize
+    super(800, 600, false)
+    self.caption = 'Hello World'
+    @background_image = Gosu::Image.new(self, "media/background.png", false)
+    @player = Player.new(self)
   end
+
+  def draw
+    @player.draw
+    @background_image.draw(0, 0, 0)
+  end
+end
 
 GameWindow.new.show
